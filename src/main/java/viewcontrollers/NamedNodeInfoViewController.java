0819d1c81517b001ab6dc3c6d7414780c960d37d
Tomasz Lewandowski <tomasz.t.lewandowski@gmail.com>
2017-01-15 03:53:38 +0100

    Named node info view

---
+++ src/main/java/viewcontrollers/NamedNodeInfoViewController.java
@@ -0,0 +1,31 @@
+package viewcontrollers;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import rdf.Node;
+import views.NamedNodeInfoView;
+
+public class NamedNodeInfoViewController extends NodeInfoViewController
+        implements ItemListener {
+
+    public NamedNodeInfoViewController() {
+        super();
+        view = new NamedNodeInfoView();
+        getNodeInfoView().getVisibilityCheckbox().addItemListener(this);
+    }
+
+    @Override
+    boolean acceptsModel(Node model) {
+        return model != null && model.getRDFNode().isURIResource();
+    }
+    
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        if (getModel() == null) return;
+        if (e.getItem() == getNodeInfoView().getVisibilityCheckbox()) {
+            getModel().setVisible(getNodeInfoView().getVisibilityCheckbox().isSelected());
+        }
+    }
+
+}
